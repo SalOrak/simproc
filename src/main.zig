b9661b07829a81d3e62a798d49c42dbb983ceb5c
PauSolerValades <pau.soler.valades@gmail.com>
2025-08-25 09:17:52 +0200

    feat(now it compiles, fixing previous commit; logic is not correct)

--- src/main.zig
+++ src/main.zig
@@ -7,19 +7,19 @@ const Cpu = struct {
     n_cores: u8, 
 };
 
-pub fn min(a: std.ArrayList) u16 {
-    var minimum: f64 = a[0]; 
-    var i: u16 = undefined; 
-    for (0..a) |j| {
-        if (minimum > a[j]){
+pub fn min(array: std.ArrayList(f64)) usize {
+    var minimum: f64 = array.items[0]; 
+    var i: usize = undefined; 
+    for (0..array.items.len) |j| {
+        if (minimum > array.items[j]){
             i = j;
-            minimum = a[j];
+            minimum = array.items[j];
         }
     } 
     return i;
 } 
 
-pub fn simulate(cpu: Cpu , tasks: []Task) f64 {
+pub fn simulate(cpu: Cpu , tasks: []Task) !f64 {
     // const n_ins: f64 = blk: {
     //     var total: u64 = 0;
     //     for (tasks) |task| { total += task.n_ins;}
@@ -27,24 +27,28 @@ pub fn simulate(cpu: Cpu , tasks: []Task) f64 {
     // };
     //
     const allocator = std.heap.page_allocator; 
-    const total_time: f64 = 0.0;
+    var total_time: f64 = 0.0;
     var cores = std.ArrayList(f64).init(allocator);
     var times_per_tasks = std.ArrayList(f64).init(allocator);
     
     for (tasks) |task| {
-        times_per_tasks.append(@as(f64, @floatFromInt(task.n_ins) / @floatFromInt(cpu.clock)));
+        const nins: f64 = @as(f64, @floatFromInt(task.n_ins));
+        const clock: f64 = @as(f64, @floatFromInt(cpu.clock));
+
+        try times_per_tasks.append(nins/clock);
     }
     
-    const items = @min(times_per_tasks.len, cpu.n_cores);
-    for (0..items) |i| {
-        cores.append(times_per_task[i]);
+    // spots as in free spaces to fill them with tasks
+    var spots = @min(times_per_tasks.items.len, cpu.n_cores);
+    for (0..spots) |i| {
+        try cores.append(times_per_tasks.items[i]);
     }
 
-    while(min < times_per_tasks.len) {
-        const min_index = min(times_per_tasks);
-        total_time += times_per_tasks[min_index];
-        items += 1;
-        times_per_tasks[min_index] = times_per_tasks[min_index];
+    while(spots < times_per_tasks.items.len) {
+        const smaller_task_index = min(cores);
+        total_time += cores.items[smaller_task_index];
+        spots += 1;
+        cores.items[smaller_task_index] = times_per_tasks.items[smaller_task_index];
     }
 
     return total_time; 
@@ -75,6 +79,6 @@ pub fn main() !void {
     };
 
 
-    std.debug.print("Result {d}\n", .{simulate(cpu, &tasks)});
+    std.debug.print("Result {!d}\n", .{simulate(cpu, &tasks)});
 
 }
